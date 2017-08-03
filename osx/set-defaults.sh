6f81a0339c3a1b17ea920d6f8474b0c0515a3d43
Hugo Melo <me@hugomelo.com>
2017-08-03 03:05:34 -0700

    tweaks

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -23,8 +23,8 @@ defaults write NSGlobalDomain KeyRepeat -int 0
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Disables itunes open on headphones button
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
+# Disables itunes open on headphones button, SIP locks this, and has to be run from recovery mode
+# launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
 
 # Disable the crash reporter
 defaults write com.apple.CrashReporter DialogType -string "none"
@@ -43,3 +43,6 @@ defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
 # Show hidden files in finder
 defaults write com.apple.finder AppleShowAllFiles YES
+
+# Auto hide the dock
+osascript -e "tell application \"System Events\" to set the autohide of the dock preferences to true"
