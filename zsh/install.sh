3dac27c6560dd29949fe4c44da14c38c728c2d93
Hugo Melo <me@hugomelo.com>
2019-02-10 19:12:28 -0500

    simplify zsh install + copy rc file

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ -n "$ZSH_VERSION" ]; then
-	echo "Zsh installed"
-elif [ -n "$BASH_VERSION" ]; then
-	echo "Installing zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Installing zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-	echo "Setting zsh as default"
-	chsh -s /bin/zsh
-fi
+echo "Setting zsh as default"
+chsh -s /bin/zsh
+
+echo "Copying oh-my-zsh config"
+[ -e ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.backup
+ln -s ~/.dotfiles/zsh/zshrc.symlink ~/.zshrc
